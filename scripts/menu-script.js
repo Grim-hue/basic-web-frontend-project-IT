a14fbd8bc85417f4af3c423c5ff986fb799ab4d7
Grim-hue <andremfreita@gmail.com>
2025-01-05 02:40:49 +0000

    added more chats and login

--- scripts/menu-script.js
+++ scripts/menu-script.js
@@ -15,17 +15,36 @@ overlay.addEventListener("click", () => {
     navBar.classList.remove("open");
 });
 
+function setDarkMode(){
+    document.querySelector("#moon-sun").classList.remove("bxs-moon") 
+    document.querySelector("#moon-sun").classList.add("bxs-sun") 
+
+}
+function removetDarkMode(){
+    document.querySelector("#moon-sun").classList.remove("bxs-sun") 
+    document.querySelector("#moon-sun").classList.add("bxs-moon") 
+    
+}
+if (localStorage.hasOwnProperty('isDarkMode')) {
+    if (localStorage.getItem('isDarkMode') === 'true') {
+        body.classList.toggle("dark-mode");
+        setDarkMode()
+    }
+ }else{
+    localStorage.setItem('isDarkMode', false);
+ }
+
 
 toggleDarkMode.addEventListener("click", () => {
-    //add or remove dark mode
-    body.classList.toggle("dark-mode");
     //chages between moon and sun icon
+    body.classList.toggle("dark-mode");
     if(document.querySelector(".dark-mode")!=null){
-        console.log(document.querySelector(".dark-mode"));
-        document.querySelector("#moon-sun").classList.remove("bxs-moon") 
-        document.querySelector("#moon-sun").classList.add("bxs-sun") 
+        localStorage.setItem('isDarkMode', 'true');
+        setDarkMode() 
     }else {
-        document.querySelector("#moon-sun").classList.remove("bxs-sun") 
-        document.querySelector("#moon-sun").classList.add("bxs-moon") 
+        localStorage.setItem('isDarkMode', 'false');
+        removetDarkMode()
     }
+
+
 });
